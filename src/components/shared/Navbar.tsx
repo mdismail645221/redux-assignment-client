026e497e861969b233a80adf70d996f5c359092a
Ismail Hossain <mdismail645221@gmail.com>
2023-09-26 22:12:56 +0600

    Added Appbar and footer component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,17 +1,17 @@
 const Navbar = () => {
     return (
-        <header className="flex items-center justify-between">
+        <header className="flex items-center justify-between bg-black text-white">
             <div className="header-left">
                 <img src="https://acedeckboards.ca/cdn/shop/files/8d8f334e7276d87a33d42ef609db5225_110x.webp?v=1679656262" alt="mian-logo" />
             </div>
             {/* header logo */}
-            <div>
-                <ul>
-                    <li> <a href="/">Home</a></li>
-                    <li><a href="/">Books Alls</a></li>
-                    <li><a href="/">Sign In </a></li>
-                    <li><a href="/">Sign Up</a></li>
-                    <li><a href="/">Log Out</a></li>
+            <div className="header-right sm:hidden md:block">
+                <ul className="flex justify-start items-center text-2xl flex-nowrap">
+                    <li className="list-none  border-r-2 border-gray-500"> <a className="px-3 text-white" href="/">Home</a></li>
+                    <li className="list-none  border-r-2 border-gray-500"><a className="px-3 text-white" href="/">Books Alls</a></li>
+                    <li className="list-none  border-r-2 border-gray-500"><a className="px-3 text-white" href="/">Sign In </a></li>
+                    <li className="list-none  border-r-2 border-gray-500"><a className="px-3 text-white" href="/">Sign Up</a></li>
+                    <li className="list-none  border-r-2 border-gray-500"><a className="px-3 text-white" href="/">Log Out</a></li>
                 </ul>
             </div>
         </header>
